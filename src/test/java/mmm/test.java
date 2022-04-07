2dabc2663592b5b1eeb25266073b65a4d64cbd23
unknown <daarlankaavinash@gmail.com>
2022-04-07 21:47:13 +0530

    first

---
+++ src/test/java/mmm/test.java
@@ -0,0 +1,20 @@
+package mmm;
+
+public class test {
+	public static void main(String[] args) {
+
+	// TODO Auto-generated method stub
+	int firstterm=0;
+	int secondterm=1;
+	for(int i=1;i<=10;i++) {
+		System.out.print(firstterm);
+
+	 	int nextterm=firstterm+secondterm;
+		firstterm=secondterm;
+		secondterm=nextterm;
+
+	}
+
+		}
+
+}
